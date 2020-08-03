42c2cc899c95ee55ed09d5dd16e9874aabd4f1ab
lfvr <9057038+lfvr@users.noreply.github.com>
2020-08-03 19:01:13 +0100

    paginate

--- network/urls.py
+++ network/urls.py
@@ -5,11 +5,14 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:page_number>", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("new_post", views.new_post, name="new_post"),
     path("profile/<str:name>", views.profile, name="profile"),
+    path("profile/<str:name>/<int:page_number>", views.profile, name="profile"),
     path("follow", views.follow, name="follow"),
-    path("following", views.following, name="following")
+    path("following", views.following, name="following"),
+    path("following/<int:page_number>", views.following, name="following")
 ]
--- network/views.py
+++ network/views.py
@@ -1,15 +1,39 @@
 from django.contrib.auth import authenticate, login, logout
+from django.core.paginator import Paginator
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
+import inspect
+
 from .models import User, Message
 
 
-def index(request):
+def index(request, page_number=1):
+    
+    # Get all messages
+    messages = Message.objects.all()
+    
+    # Paginate
+    p = Paginator(messages, 10)
+    num_pages = p.num_pages
+   
+    # Return max 10 messages
+    if num_pages > 1:
+        messages = p.page(page_number).object_list
+
+    # Check min and max
+    previous = page_number - 1
+    next = page_number + 1
+
     return render(request, "network/index.html", {
-        "messages": Message.objects.all()
+        "messages": messages,
+        "num_pages": num_pages,
+        "page_range": p.page_range,
+        "curr_page": page_number,
+        "previous": previous,
+        "next": next
     })
 
 
@@ -72,11 +96,9 @@ def new_post(request):
     Message.objects.create(user=user, message=message)
 
     # Return to posts
-    return render(request, "network/index.html", {
-        "messages": Message.objects.all()
-    })
+    return HttpResponseRedirect(reverse("index"))
 
-def profile(request, name):
+def profile(request, name, page_number=1):
 
     # Get user, messages and follow counts
     curr_user = request.user
@@ -91,6 +113,23 @@ def profile(request, name):
     # Check if currently following
     is_following = User.objects.filter(username=curr_user.username, following__username=user).exists()
 
+    num_pages = 0
+    page_range = range(0)
+    curr_page = 0
+    previous = 0
+    next = 0
+
+    # Page messages if necessary
+    if len(messages) > 10:
+        dict = make_pages(messages, page_number)
+        messages = dict["messages"]
+        num_pages = dict["num_pages"]
+        page_range = dict["page_range"]
+        curr_page: dict["curr_page"]
+        previous = dict["previous"]
+        next = dict["next"]
+
+
     # Show profile page
     return render(request, "network/profile.html", {
         "is_curr_user": is_curr_user,
@@ -98,15 +137,42 @@ def profile(request, name):
         "messages": messages,
         "following": following_count,
         "followers": followers_count,
-        "is_following": is_following
+        "is_following": is_following,
+        "num_pages": num_pages,
+        "page_range": page_range,
+        "curr_page": curr_page,
+        "previous": previous,
+        "next": next
     })
 
-def following(request):
+def following(request, page_number=1):
     # get list of following, then get their messages
     following = User.objects.values('following').filter(username=request.user)
     messages = Message.objects.filter(user__in=following)
+
+    num_pages = 0
+    page_range = range(0)
+    curr_page = 0
+    previous = 0
+    next = 0
+
+    # Page messages if necessary
+    if len(messages) > 10:
+        dict = make_pages(messages, page_number)
+        messages = dict["messages"]
+        num_pages = dict["num_pages"]
+        page_range = dict["page_range"]
+        curr_page: dict["curr_page"]
+        previous = dict["previous"]
+        next = dict["next"]
+
     return render(request, "network/index.html", {
-        "messages": messages
+        "messages": messages,
+        "num_pages": num_pages,
+        "page_range": page_range,
+        "curr_page": curr_page,
+        "previous": previous,
+        "next": next
     })
 
 def follow(request):
@@ -125,3 +191,25 @@ def follow(request):
         curr_user.following.add(profile)
     return JsonResponse({"success": True})
 
+def make_pages(messages, page_number):
+    # Paginate
+    p = Paginator(messages, 10)
+    num_pages = p.num_pages
+   
+    # Return max 10 messages
+    if num_pages > 1:
+        messages = p.page(page_number).object_list
+
+    # Check min and max
+    previous = page_number - 1
+    next = page_number + 1
+
+    # Return values
+    return {
+        "messages": messages,
+        "num_pages": num_pages,
+        "page_range": p.page_range,
+        "curr_page": page_number,
+        "previous": previous,
+        "next": next
+    }
